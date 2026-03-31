af9f9c4df7ddd2b9206cfa0cdb6014ebd621c1b6
Peter Bozsoky <peter-bozsoky@posteo.de>
2026-03-31 19:45:29 +0200

    generator: Make header-content-footer sandwiches

--- rebuild/build.py
+++ rebuild/build.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 
 SRC_PATH = "./src"
 REFERENCES_PATH = "./references"
-HEADER_FILE_NAME = "./header.html"
+HEADER_FILE_NAME = "header.html"
 FOOTER_FILE_NAME = "footer.html"
 
 
@@ -39,20 +39,23 @@ def list_content_files():
 def list_all_build_files() -> list[str]:
     return os.listdir(SRC_PATH)
 
+def enrich(content, header="", footer=""):
+    return "\n".join([header, content, footer])
+
 if __name__ == "__main__":
     all_build_files = list_all_build_files()
     content_files = list_content_files()
-    header_file_content = read_header()
-    footer_file_content = read_footer()
+    header = read_header()
+    footer = read_footer()
 
     log.info(f"*** BUILD DIR ***********\n{all_build_files}")
-    log.info(f"*** HEADER CONTENT ******\n{header_file_content}")
-    log.info(f"*** FOOTER CONTENT ******\n{footer_file_content}")
+    log.info(f"*** HEADER CONTENT ******\n{header}")
+    log.info(f"*** FOOTER CONTENT ******\n{footer}")
     log.info(f"*** CONTENT FILES ******\n{content_files}")
 
     for filename in content_files:
         log.info(f"Processing {filename}")
         body_content = get_body(filename)
-        log.info(f"*** PURE CONTENT ****** \n{body_content}")
+        log.info(enrich(body_content, header, footer))
 
     log.info("*************************")
