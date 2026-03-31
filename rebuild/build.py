54eb27b7b8f3a06e1276ba22abbd961b5f18235b
Peter Bozsoky <peter-bozsoky@posteo.de>
2026-03-31 19:40:39 +0200

    generator: Proper main

--- rebuild/build.py
+++ rebuild/build.py
@@ -39,20 +39,20 @@ def list_content_files():
 def list_all_build_files() -> list[str]:
     return os.listdir(SRC_PATH)
 
+if __name__ == "__main__":
+    all_build_files = list_all_build_files()
+    content_files = list_content_files()
+    header_file_content = read_header()
+    footer_file_content = read_footer()
 
-all_build_files = list_all_build_files()
-content_files = list_content_files()
-header_file_content = read_header()
-footer_file_content = read_footer()
+    log.info(f"*** BUILD DIR ***********\n{all_build_files}")
+    log.info(f"*** HEADER CONTENT ******\n{header_file_content}")
+    log.info(f"*** FOOTER CONTENT ******\n{footer_file_content}")
+    log.info(f"*** CONTENT FILES ******\n{content_files}")
 
-log.info(f"*** BUILD DIR ***********\n{all_build_files}")
-log.info(f"*** HEADER CONTENT ******\n{header_file_content}")
-log.info(f"*** FOOTER CONTENT ******\n{footer_file_content}")
-log.info(f"*** CONTENT FILES ******\n{content_files}")
+    for filename in content_files:
+        log.info(f"Processing {filename}")
+        body_content = get_body(filename)
+        log.info(f"*** PURE CONTENT ****** \n{body_content}")
 
-for filename in content_files:
-    log.info(f"Processing {filename}")
-    body_content = get_body(filename)
-    log.info(f"*** PURE CONTENT ****** \n{body_content}")
-
-log.info("*************************")
+    log.info("*************************")
