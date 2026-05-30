e19279fd5a54130948c6f3ee288de86b88d8576a
Peter Bozsoky <peter-bozsoky@posteo.de>
2026-05-30 13:49:11 +0200

    builder: Added tests, one failing

--- rebuild/build.py
+++ rebuild/build.py
@@ -6,7 +6,6 @@ from AdvancedHTMLParser import AdvancedHTMLParser
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-
 SRC_PATH = "./src"
 REFERENCES_PATH = "./references"
 HEADER_FILE_NAME = "header.html"
@@ -15,25 +14,18 @@ FOOTER_FILE_NAME = "footer.html"
 
 def get_body(filename):
     parser = AdvancedHTMLParser()
-    parser.parseFile(path.join(SRC_PATH, filename))
+    p = path.join(".", filename)
+    parser.parseFile(p)
     body = parser.body
     return str(body.text.strip())
 
 
 def read_header():
-    return get_body(HEADER_FILE_NAME)
+    return get_body("/".join((SRC_PATH, HEADER_FILE_NAME)))
 
 
 def read_footer():
-    return get_body(FOOTER_FILE_NAME)
-
-
-def list_content_files():
-    return [
-        filename
-        for filename in all_build_files
-        if filename != HEADER_FILE_NAME and filename != FOOTER_FILE_NAME
-    ]
+    return get_body("/".join((SRC_PATH, FOOTER_FILE_NAME)))
 
 
 def list_all_build_files() -> list[str]:
@@ -44,7 +36,14 @@ def enrich(content, header="", footer=""):
     return "\n".join([header, content, footer])
 
 
-if __name__ == "__main__":
+def build_website():
+    def list_content_files():
+        return [
+            filename
+            for filename in all_build_files
+            if filename != HEADER_FILE_NAME and filename != FOOTER_FILE_NAME
+        ]
+
     all_build_files = list_all_build_files()
     content_files = list_content_files()
     header = read_header()
@@ -56,8 +55,12 @@ if __name__ == "__main__":
     log.info(f"*** CONTENT FILES ******\n{content_files}")
 
     for filename in content_files:
-        body_content = get_body(filename)
+        body_content = get_body(path.join(SRC_PATH, filename))
         enriched = enrich(body_content, header, footer)
         log.info(f"Processed {filename}\n{enriched}")
 
     log.info("*************************")
+
+
+if __name__ == "__main__":
+    build_website()
--- rebuild/test_build.py
+++ rebuild/test_build.py
@@ -1,3 +1,17 @@
-def test_something():
-        assert True
+import build
 
+def test_setup_works():
+    assert True
+
+
+def test_enrich():
+    expected = "a\nb\nc"
+
+    encountered = build.enrich("b", "a", "c")
+
+    assert expected == encountered
+
+def test_body_extraction():
+    expected = "foo"
+    encountered = build.get_body("test_resources/hello.html")
+    assert expected == encountered
